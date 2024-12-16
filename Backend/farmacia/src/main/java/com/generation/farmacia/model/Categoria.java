accc8a91d429cd97758d7fbd7dbb28ab02cba0bc
Caiqe <cttcaiquegomes@gmail.com>
2024-12-16 12:56:34 -0300

    Criando CRUD Categoria

---
+++ Backend/farmacia/src/main/java/com/generation/farmacia/model/Categoria.java
@@ -0,0 +1,39 @@
+package com.generation.farmacia.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_categorias")
+public class Categoria {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	@NotBlank(message = "O atributo 'descricao' deve ser preenchido")
+	@Size(min = 1, max = 255, message = "O atributo 'descricao' deve ter entre 1 e 255 caracteres")
+	private String descricao;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+}
